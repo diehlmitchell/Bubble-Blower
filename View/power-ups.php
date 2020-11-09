f5ce5754ca8ae50c87d4d2f647e7ead2363880f9
Mitchell Diehl <mdiehl@training.local>
2020-11-09 14:53:13 -0500

    added a dirty buble

--- View/power-ups.php
+++ View/power-ups.php
@@ -1,20 +1,21 @@
 <link rel="stylesheet" href = "./View/Public/Styles/power-up-styles.css">
 
 <?php
-$powerUpsArray = array (    // array("Actual Image Name","Alt","Display Name", amount)
-    array("squid-bubble.jpg","Squid-Bubble image", "Squidward",0),  
-    array("patrick-bubble.jpg","patrick-bubble image", "Patrick",0),  
-    array("sponge-bubble.jpg","sponge-bubble image", "Sponge-bob",0),
-    array("hitman-bubble.jpg","hitman-bubble image", "Hitman",0),
-    array("suds-bob.jpg","suds-bob image", "Suds-Bob",0),
-    array("jugg-bubble.jpg","jugg-bubble image", "Bubble juggernog",0),
-);  //end of the power ups array
+    $powerUpsArray = array (  // array("Actual Image Name","Alt","Display Name", amount)
+        array("squid-bubble.jpg","Squid-Bubble image", "Squidward",0),  
+        array("patrick-bubble.jpg","patrick-bubble image", "Patrick",0),  
+        array("sponge-bubble.jpg","sponge-bubble image", "Sponge-bob",0),
+        array("hitman-bubble.jpg","hitman-bubble image", "Hitman",0),
+        array("suds-bob.jpg","suds-bob image", "Suds-Bob",0),
+        array("dirty-bubble.jpg","dirty-bubble image", "Dirty Bubble",0),
+        array("jugg-bubble.jpg","jugg-bubble image", "Bubble juggernog",0)
+    );  //end of the power ups array
 
 ?>
 
 <div class ="power-ups-grid">
     <?php 
-        for($X = 0; $X < 6;$X++)
+        for($X = 0; $X < count($powerUpsArray);$X++)
         {
             echo"
                 <div class = 'power-up'>
