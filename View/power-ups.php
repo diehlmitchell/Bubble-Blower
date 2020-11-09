4b96c351774362f93be5762ecde8b9738725fbb5
Mitchell Diehl <mdiehl@training.local>
2020-11-09 13:31:03 -0500

    power-ups is mainly complete, may need tweaks

--- View/power-ups.php
+++ View/power-ups.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href = "./Public/CSS/power-up-styles.css">
+
 <?php
 $powerUpsArray = array (    // array("Actual Image Name","Alt","Display Name", amount)
     array("squid-bubble.jpg","Squid-Bubble image", "Squidward",0),  
@@ -5,7 +7,7 @@ $powerUpsArray = array (    // array("Actual Image Name","Alt","Display Name", a
     array("sponge-bubble.jpg","sponge-bubble image", "Sponge-bob",0),
     array("hitman-bubble.jpg","hitman-bubble image", "Hitman",0),
     array("suds-bob.jpg","suds-bob image", "Suds-Bob",0),
-    array("jugg-bubble.jpg","jugg-bubble image", "The juggernog",0),
+    array("jugg-bubble.jpg","jugg-bubble image", "Bubble juggernog",0),
 );  //end of the power ups array
 
 ?>
@@ -15,17 +17,19 @@ $powerUpsArray = array (    // array("Actual Image Name","Alt","Display Name", a
         for($X = 0; $X < 6;$X++)
         {
             echo"
-            <img src = "./Public/Images/Power-Ups/(powerUpsArray[$X][0])" alt="(powerUpsArray[$X][1])">
-            <p>(powerUpsArray[$X][2])</p>
-            <p>(powerUpsArray[$X][3])</p>
+                <div class = 'power-up'>
+                    <img src='./Public/Images/Power-Ups/{$powerUpsArray[$X][0]}'>
+                    <p>{$powerUpsArray[$X][2]}</p>
+                    <p>{$powerUpsArray[$X][3]}</p>
+                </div>
             ";
         }
     ?>
 
-    <div class = "power-up">
+    <!-- <div class = "power-up">
             <img src = "./Public/Images/Power-Ups/squid-bubble.jpg" alt="squid-bubble.jpg">
             <p> Squid Bubble</p>
             <p>0</p>
-    </div>
+    </div> -->
 
 </div>
