721ac9389d1df069317707479cb8c7fe76872275
Mitchell Diehl <mdiehl@training.local>
2020-11-09 15:47:23 -0500

    added some comments that would be useful for later

--- View/power-ups.php
+++ View/power-ups.php
@@ -1,14 +1,14 @@
 <link rel="stylesheet" href = "./View/Public/Styles/power-up-styles.css">
 
 <?php
-    $powerUpsArray = array (  // array("Actual Image Name","Alt","Display Name", amount)
-        array("squid-bubble.jpg","Squid-Bubble image", "Squidward",0),  
-        array("patrick-bubble.jpg","patrick-bubble image", "Patrick",0),  
-        array("sponge-bubble.jpg","sponge-bubble image", "Sponge-bob",0),
-        array("hitman-bubble.jpg","hitman-bubble image", "Hitman",0),
-        array("suds-bob.jpg","suds-bob image", "Suds-Bob",0),
-        array("dirty-bubble.jpg","dirty-bubble image", "Dirty Bubble",0),
-        array("jugg-bubble.jpg","jugg-bubble image", "Bubble juggernog",0)
+    $powerUpsArray = array (  // array("Actual Image Name","Alt","Display Name", amount,Cost)
+        array("squid-bubble.jpg","Squid-Bubble image", "Squidward",1,10),  
+        array("patrick-bubble.jpg","patrick-bubble image", "Patrick",0,100),  
+        array("sponge-bubble.jpg","sponge-bubble image", "Sponge-bob",0,500),
+        array("hitman-bubble.jpg","hitman-bubble image", "Hitman",0,3000),
+        array("suds-bob.jpg","suds-bob image", "Suds-Bob",0,10000),
+        array("dirty-bubble.png","dirty-bubble image", "Dirty Bubble",0,10000),
+        array("jugg-bubble.jpg","jugg-bubble image", "Bubble jugg",0,100000)
     );  //end of the power ups array
 
 ?>
@@ -20,8 +20,10 @@
             echo"
                 <div class = 'power-up'>
                     <img src='./View/Public/Images/Power-Ups/{$powerUpsArray[$X][0]}'>
-                    <p>{$powerUpsArray[$X][2]}</p>
-                    <p>{$powerUpsArray[$X][3]}</p>
+                    <div class ='power-up-text'>  //onclick='addPower({$powerUpsArray[$X][3]})'
+                        <p>{$powerUpsArray[$X][2]}    amount:{$powerUpsArray[$X][3]}</p>
+                        <p>Bubble cost: (Cost)</p>
+                    </div>
                 </div>
             ";
         }
@@ -32,5 +34,15 @@
             <p> Squid Bubble</p>
             <p>0</p>
     </div> -->
+
+    <!-- Notes and idea; to save time or effort, switch case based on the $X that adds to the $powerUpsArray[$X][3]
+     amount and subtracts cost from the current bubble amount
+    (if theres enough bubbles), which would be "$powerUpsArray[$X][4] < score" 
+    ---Must be in javascript because PHP will not communicate with the array after it started (or something like that idk)
+    -->
+    
+        
+
+
 
 </div>
