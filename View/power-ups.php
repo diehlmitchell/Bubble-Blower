81051ac62c521fcc7840d836bcc890020e560c43
Mitchell Diehl <mdiehl@training.local>
2020-11-10 09:49:41 -0500

    added comments, small display changes

--- View/power-ups.php
+++ View/power-ups.php
@@ -1,22 +1,21 @@
 <link rel="stylesheet" href = "./View/Public/Styles/power-up-styles.css">
 
 <?php
-    $powerUpsArray = array (  // array("Actual Image Name","Alt","Display Name", amount,Cost)
-        array("squid-bubble.jpg","Squid-Bubble image", "Squidward",1,10),  
-        array("patrick-bubble.jpg","patrick-bubble image", "Patrick",0,100),  
-        array("sponge-bubble.jpg","sponge-bubble image", "Sponge-bob",0,500),
-        array("hitman-bubble.jpg","hitman-bubble image", "Hitman",0,3000),
-        array("suds-bob.jpg","suds-bob image", "Suds-Bob",0,10000),
-        array("dirty-bubble.png","dirty-bubble image", "Dirty Bubble",0,10000),
-        array("jugg-bubble.jpg","jugg-bubble image", "Bubble jugg",0,100000)
+    $powerUpsArray = array (  // array("Actual Image Name","Alt","Display Name", amount,Cost, bubbles per second)
+        array("squid-bubble.jpg","Squid-Bubble image", "Squidward",1,10, 0.2),  
+        array("patrick-bubble.jpg","patrick-bubble image", "Patrick",0,40, .5),  
+        array("sponge-bubble.jpg","sponge-bubble image", "Sponge-bob",9,100, 1),
+        array("hitman-bubble.jpg","hitman-bubble image", "Hitman",0,750, 3),
+        array("suds-bob.jpg","suds-bob image", "Suds-Bob",0,3000, 15),
+        array("dirty-bubble.png","dirty-bubble image", "Dirty Bubble",0,900, 50),
+        array("jugg-bubble.jpg","jugg-bubble image", "Bubble jugg",11,100000, 150)
     );  //end of the power ups array
 
 ?>
 
 <div class ="power-ups-grid">
     <?php
-        // $CurrentPrice;
-        
+        // $CurrentPrice;        
         for($X = 0; $X < count($powerUpsArray);$X++)
         {
             $CurrentPrice = round($powerUpsArray[$X][4]*(1.15**$powerUpsArray[$X][3]));
@@ -24,10 +23,8 @@
                 <div class = 'power-up'>
                     <img src='./View/Public/Images/Power-Ups/{$powerUpsArray[$X][0]}'>
                     <div class ='power-up-text'>  
-                        <p>{$powerUpsArray[$X][2]}    amount:{$powerUpsArray[$X][3]}</p>     
-                        <p>Bubble Cost: $CurrentPrice</p>     
-                        
-                        
+                        <p>{$powerUpsArray[$X][2]}   <br> amount:{$powerUpsArray[$X][3]} <br> BPS:{$powerUpsArray[$X][5]} </p>     
+                        <p>Bubble Cost: $CurrentPrice</p>               
                     </div>
                 </div>
             ";
@@ -39,6 +36,8 @@
             <p> Squid Bubble</p>
             <p>0</p>
     </div> -->
+
+    <!------------------------------- IDEAS ------------------------------------------- AND STUFF --------------------------------------------------------->
 <!--CurrentPrice = BaseCost*(1.15**{$powerUpsArray[$X][3]}) -->
     <!-- Notes and ideas; to save time or effort, switch case based on the $X that adds to the $powerUpsArray[$X][3]
      amount and subtracts cost from the current bubble amount
@@ -46,8 +45,11 @@
     ---Must be in javascript because PHP will not communicate with the array after it started (or something like that idk)
     -->
     
-        
-
-
+        <!--If we have additional worktime: Add a way to grab and store system time,
+         when you start to play again it grabs a new system time and calculates bubbles per second and seconds passed, adds your new score -->
+         <!-- <p>Date/Time: <span id="datetime"></span></p>
+         var dt = new Date();
+         document.getElementById("datetime").innerHTML = dt.toLocaleString(); -->
+    <!------------------------------- IDEAS ------------------------------------------- AND STUFF --------------------------------------------------------->
 
 </div>
